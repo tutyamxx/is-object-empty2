3750f5658965e6d1350cda110684601351665995
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-04-06 20:37:54 +0100

    UPDATE: Updated workflow

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  is-object-empty2 - 📦 Tiny utility to check if a value is a plain empty object in JavaScript and TypeScript
- *  @version: v1.1.1
+ *  @version: v1.1.2
  *  @link: https://github.com/tutyamxx/is-object-empty2
  *  @license: MIT
  **/
